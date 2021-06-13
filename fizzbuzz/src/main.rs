89e82ec4e0c7774c7753feb3f9ddc9bf5e043aaf
rfrazier <rfrazier716@gmail.com>
2021-06-13 15:00:48 -0400

    added red-green unit tests

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,4 +1,8 @@
 // Did not know this comes from a children's game https://en.wikipedia.org/wiki/Fizz_buzz
+fn fizzbuzz(num: i32) -> String{
+    String::from("Hello")
+}
+
 fn main() {
     for num in 1..101 {
         if (num % 3 == 0) && (num % 5 == 0){
@@ -9,5 +13,25 @@ fn main() {
         } else if num % 5 == 0{
             println!("buzz")
         } else {println!("{}",num)}
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_fizz(){
+        assert_eq!(fizzbuzz(36), String::from("fizz"))
+    }
+
+    #[test]
+    fn test_buzz(){
+        assert_eq!(fizzbuzz(25), String::from("buzz"))
+    }
+
+    #[test]
+    fn test_fizzbuzz(){
+        assert_eq!(fizzbuzz(30), String::from("fizzbuzz"))
     }
 }
