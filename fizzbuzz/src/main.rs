88ec3368dbfec519a53258bcf06a64aede937363
rfrazier <rfrazier716@gmail.com>
2021-06-13 15:23:23 -0400

    fizzbuzz with enum and display trait

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,10 +1,37 @@
 // Did not know this comes from a children's game https://en.wikipedia.org/wiki/Fizz_buzz
-fn fizzbuzz(num: i32) -> String {
+use core::fmt;
+use std::fmt::Formatter;
+
+// Rust enums allow for contained values (this is exactly what Result and Option do)
+// Fizzbuzz is an opportunity to show it, since the num case can contain the actual value
+#[derive(Debug, PartialEq)]
+enum FizzBuzz{
+    Fizz,
+    Buzz,
+    FizzBuzz,
+    Num(i32)
+}
+
+// Implementing display lets us print the result
+// They all need to be strings, since calling format! on val has unknown stack size at compile time
+impl fmt::Display for FizzBuzz{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let display = match self{
+            FizzBuzz::Fizz => String::from("fizz"),
+            FizzBuzz::Buzz => String::from("buzz"),
+            FizzBuzz::FizzBuzz => String::from("fizzbuzz"),
+            FizzBuzz::Num(val) => format!("{}", val)
+        };
+        write!(f, "{}", display)
+    }
+}
+
+fn fizzbuzz(num: i32) -> FizzBuzz {
     match (num % 3, num % 5) {
-        (0, 0) => String::from("fizzbuzz"),
-        (0, _) => String::from("fizz"),
-        (_, 0) => String::from("buzz"),
-        (_, _) => format!("{}", num),
+        (0, 0) => FizzBuzz::FizzBuzz,
+        (0, _) => FizzBuzz::Fizz,
+        (_, 0) => FizzBuzz::Buzz,
+        (_, _) => FizzBuzz::Num(num),
     }
 }
 
@@ -20,16 +47,21 @@ mod tests {
 
     #[test]
     fn test_fizz() {
-        assert_eq!(fizzbuzz(36), String::from("fizz"))
+        assert_eq!(fizzbuzz(36), FizzBuzz::Fizz)
     }
 
     #[test]
     fn test_buzz() {
-        assert_eq!(fizzbuzz(25), String::from("buzz"))
+        assert_eq!(fizzbuzz(25), FizzBuzz::Buzz)
     }
 
     #[test]
     fn test_fizzbuzz() {
-        assert_eq!(fizzbuzz(30), String::from("fizzbuzz"))
+        assert_eq!(fizzbuzz(30), FizzBuzz::FizzBuzz)
+    }
+
+    #[test]
+    fn test_num() {
+        assert_eq!(fizzbuzz(7), FizzBuzz::Num(7))
     }
 }
