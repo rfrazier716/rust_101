fc75e924433e58dee93a1f32512f1ea62cdd8355
rfrazier <rfrazier716@gmail.com>
2021-06-13 14:57:07 -0400

    basic fizzbuzz with if statements

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,3 +1,13 @@
+// Did not know this comes from a children's game https://en.wikipedia.org/wiki/Fizz_buzz
 fn main() {
-    println!("Hello, world!");
+    for num in 1..101 {
+        if (num % 3 == 0) && (num % 5 == 0){
+            println!("fizzbuzz")
+        }
+        else if num % 3 == 0{
+            println!("fizz")
+        } else if num % 5 == 0{
+            println!("buzz")
+        } else {println!("{}",num)}
+    }
 }
